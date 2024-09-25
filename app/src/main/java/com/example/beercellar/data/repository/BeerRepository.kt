de0f03e85ec47c48ca946f4cc8801681b72df5cb
Frederik Brauer Nissen - HjemmePC <fred145a@edu.easj.dk>
2024-09-25 21:48:38 +0200

    added beerrepository

---
+++ app/src/main/java/com/example/beercellar/data/repository/BeerRepository.kt
@@ -0,0 +1,19 @@
+package com.example.beercellar.data.repository
+
+class BeerRepository {
+    fun getAllBeers() {
+
+    }
+
+    fun addBeer() {
+
+    }
+
+    fun deleteBeer() {
+
+    }
+
+    fun sortBeers() {
+
+    }
+}
